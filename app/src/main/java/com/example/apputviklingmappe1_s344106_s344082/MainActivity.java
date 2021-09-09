c6f19390bd0692e0bfb4dbf648f66f9d2a801f8d
WilliamNerem <william-kn@hotmail.com>
2021-09-09 13:26:16 +0200

    Lagt til knapper og linka til ny side for start spill

--- app/src/main/java/com/example/apputviklingmappe1_s344106_s344082/MainActivity.java
+++ app/src/main/java/com/example/apputviklingmappe1_s344106_s344082/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.apputviklingmappe1_s344106_s344082;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_start = (Button)findViewById(R.id.button_start);
+
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StartSpill.class));
+            }
+        });
     }
+
+
+
+
+
 }
