7256210f295c947708348eb7fa4dc30c67fba158
WilliamNerem <william-kn@hotmail.com>
2021-09-09 15:23:12 +0200

    Lagt til ny linear layout i start spill

--- app/src/main/java/com/example/apputviklingmappe1_s344106_s344082/StartSpill.java
+++ app/src/main/java/com/example/apputviklingmappe1_s344106_s344082/StartSpill.java
@@ -12,17 +12,17 @@ public class StartSpill extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_spill);
+        final Button button_1 = (Button)findViewById(R.id.button_1);
+        final Button button_2 = (Button)findViewById(R.id.button_2);
+        final Button button_3 = (Button)findViewById(R.id.button_3);
+        final Button button_4 = (Button)findViewById(R.id.button_4);
+        final Button button_5 = (Button)findViewById(R.id.button_5);
+        final Button button_6 = (Button)findViewById(R.id.button_6);
+        final Button button_7 = (Button)findViewById(R.id.button_7);
+        final Button button_8 = (Button)findViewById(R.id.button_8);
+        final Button button_9 = (Button)findViewById(R.id.button_9);
+        final Button button_0 = (Button)findViewById(R.id.button_0);
     }
-    final Button button_1 = (Button)findViewById(R.id.button_1);
-    final Button button_2 = (Button)findViewById(R.id.button_2);
-    final Button button_3 = (Button)findViewById(R.id.button_3);
-    final Button button_4 = (Button)findViewById(R.id.button_4);
-    final Button button_5 = (Button)findViewById(R.id.button_5);
-    final Button button_6 = (Button)findViewById(R.id.button_6);
-    final Button button_7 = (Button)findViewById(R.id.button_7);
-    final Button button_8 = (Button)findViewById(R.id.button_8);
-    final Button button_9 = (Button)findViewById(R.id.button_9);
-    final Button button_0 = (Button)findViewById(R.id.button_0);
 
 
 }
